351081041471c26d4bf48c44667ff47b8f163429
OleslieO <leslietan0521@gmail.com>
2025-12-17 00:10:05 +0800

    Update ServoTest.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/ServoTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/ServoTest.java
@@ -16,30 +16,44 @@ public class ServoTest extends LinearOpMode {
     private final Telemetry telemetry_M =
             new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
     public static boolean read_only = false;
-    public static boolean reverse = false;
+    public static boolean reverse0 = false;
+
+    public static boolean reverse1 = false;
     public static double servo_pos = 0.5;
 
-    public static String input_name = "claw_in";
-    public static String servo_name = "claw";
+
+    public static String servo_name0 = "spinUp";
+    public static String servo_name1 = "spinDown";
+
+
 
     @Override
     public void runOpMode() {
 
-        Servo servo0 = hardwareMap.get(Servo.class, servo_name);
-        AnalogInput input = hardwareMap.get(AnalogInput.class, input_name);
+        Servo servo0 = hardwareMap.get(Servo.class, servo_name0);
 
-        if (reverse) {
+        Servo servo1 = hardwareMap.get(Servo.class, servo_name1);
+
+
+        if (reverse0) {
             servo0.setDirection(Servo.Direction.REVERSE);
         }
+
+        if (reverse1) {
+            servo1.setDirection(Servo.Direction.REVERSE);
+        }
+
 
         waitForStart();
 
         while (opModeIsActive()) {
-            if (!read_only) servo0.setPosition(servo_pos);
-            telemetry_M.addData(servo_name, servo0.getPosition());
-            telemetry_M.addData(input_name, input.getVoltage());
-            telemetry_M.addData("max v", input.getMaxVoltage());
-            telemetry_M.addData("Axon degree", input.getVoltage()/3.3*360);
+            if (!read_only){
+                servo0.setPosition(servo_pos);
+                servo1.setPosition(servo_pos);
+            }
+            telemetry_M.addData(servo_name0, servo0.getPosition());
+            telemetry_M.addData(servo_name1, servo1.getPosition());
+
             telemetry_M.update();
         }
     }
